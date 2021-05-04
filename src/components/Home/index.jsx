7e89372d29505b5b3c744e101079d6326620e23c
OdongAlican <sandieo.2020@gmail.com>
2021-05-04 17:02:31 +0300

    Add the following - Install project npm packages - Setup redux store - Remove redundant files - setup linter files - Create components folder in src folder

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <p>Home component</p>
+  </div>
+);
+
+export default Home;
---
+++ src/components/Router/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Router = () => (
+  <div>
+    <p>Router file</p>
+  </div>
+);
+
+export default Router;
