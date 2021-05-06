00c815186caefad1aed7a59f487844f87f208f96
OdongAlican <sandieo.2020@gmail.com>
2021-05-07 01:43:49 +0300

    import staticData reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import individualCustomersReducer from './individualCustomers';
+import staticDataReducer from './staticData';
 
 const rootReducer = combineReducers({
   individualCustomersReducer,
+  staticDataReducer,
 });
 
 export default rootReducer;
