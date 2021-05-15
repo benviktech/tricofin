7c1243295453882e2d1d60a4346ff2d2b9e6362c
OdongAlican <sandieo.2020@gmail.com>
2021-05-15 16:50:59 +0300

    add Identification component

---
+++ src/components/Pages/Indentification.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ModalFunction from '../Modal/ModalFunction';
+import Modal from '../Modal/Modal';
+import Sidebar from '../Sidebar/Sidebar';
+import MoreInfo from '../Customer/MoreInfo';
+import '../Customer/index.css';
+
+const Indentification = () => {
+  const {
+    modalCloser, modalOpener, openModel, modalText,
+  } = ModalFunction();
+
+  return (
+    <div className="individual-customer-form">
+      <Modal
+        modalText={modalText}
+        modalCloser={modalCloser}
+        openModel={openModel}
+      />
+      <div className="lower-form-section">
+        <div className="maintenance-customer-info">
+          <span>Customer Signature and Photo Section</span>
+        </div>
+        <div className="lower-downer-section">
+          <div className="left-inner-form-section">
+            <Sidebar />
+          </div>
+          <div className="submit-form-top-section">
+            <div className="signature-section">
+              <p>Identification section</p>
+            </div>
+            <MoreInfo modalOpener={modalOpener} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Indentification;
