0bd6ac67846e5350f7a00f941696e5233f43e4e1
OdongAlican <sandieo.2020@gmail.com>
2021-05-19 01:09:40 +0300

    update layout

--- src/components/Sidebar/SidebarMenu.jsx
+++ src/components/Sidebar/SidebarMenu.jsx
@@ -3,7 +3,8 @@
 /* eslint-disable react/no-array-index-key */
 
 import React, { useState } from 'react';
-import { NavLink, useRouteMatch } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { NavLink, useParams, useRouteMatch } from 'react-router-dom';
 import styled from 'styled-components';
 
 const SidebarLink = styled.div`
@@ -28,7 +29,7 @@ const SidebarLabel = styled.span`
 
 const DropdownLink = styled(NavLink)`
     height: 60px;
-    padding-left: 3rem;
+    padding-left: 2rem;
     display: flex;
     align-items: center;
     color: #444;
@@ -47,15 +48,17 @@ const DropdownLinkIcon = styled.div`
 
 const SidebarMenu = ({ item }) => {
   const [subnav, setSubnav] = useState(false);
+  const { id } = useParams();
 
   const showSubnav = () => setSubnav(!subnav);
   const { url } = useRouteMatch();
-  console.log(url, 'url information');
+
+  const currentUser = useSelector(state => state.individualCustomersReducer);
 
   React.useEffect(() => {
-    if (url === '/signature'
-    || url === '/identification'
-    || url === '/contactinfo') {
+    if (url.split('/')[1] === 'signature'
+    || url.split('/')[1] === 'identification'
+    || url.split('/')[1] === 'contactinfo') {
       setSubnav(true);
     }
   }, []);
@@ -75,15 +78,19 @@ const SidebarMenu = ({ item }) => {
         </div>
       </SidebarLink>
       {
+        Object.keys(currentUser.individualCustomer).length > 0
+        || url.split('/')[1] === 'signature'
+        || url.split('/')[1] === 'identification'
+        || url.split('/')[1] === 'contactinfo' ? (
+
             subnav && item.subNav.map((item, index) => (
               <DropdownLink
                 onClick={item.subNav && showSubnav}
                 activeStyle={{
                   background: '#AEAEAE',
-                  color: '#fff',
                 }}
                 exact
-                to={item.path}
+                to={`${item.path}/${id}`}
                 key={index}
               >
                 <DropdownLinkIcon>
@@ -92,6 +99,7 @@ const SidebarMenu = ({ item }) => {
                 <SidebarLabel>{ item.title }</SidebarLabel>
               </DropdownLink>
             ))
+          ) : null
         }
     </>
   );
