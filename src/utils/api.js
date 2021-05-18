c8e9027fd674982e1f676e7206fb8b29b556ad75
OdongAlican <sandieo.2020@gmail.com>
2021-05-19 01:11:05 +0300

    update utils

--- src/utils/api.js
+++ src/utils/api.js
@@ -31,3 +31,13 @@ export const FetchStaticData = async (method, path) => {
   const response = await axios[method](`${baseUrl}/${path}`);
   return response;
 };
+
+export const SignaturePhotoAdditionRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
+
+export const PhotoSignatureRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
