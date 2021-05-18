8e878cfe3c0749569e1923e189bb3d11a3ce6eb3
OdongAlican <sandieo.2020@gmail.com>
2021-05-19 01:08:02 +0300

    update layout

--- src/components/Pages/Signature.jsx
+++ src/components/Pages/Signature.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import ModalFunction from '../Modal/ModalFunction';
-import Modal from '../Modal/Modal';
+import { useHistory, useParams } from 'react-router';
+import { useDispatch } from 'react-redux';
 import Sidebar from '../Sidebar/Sidebar';
-import MoreInfo from '../Customer/MoreInfo';
 import '../Customer/index.css';
+import './index.css';
+import { SignaturePhotoAddition } from '../../actions/individualCustomer';
 
 const Signature = () => {
-  const {
-    modalCloser, modalOpener, openModel, modalText,
-  } = ModalFunction();
+  const [photo, setPhoto] = React.useState(null);
+  const [signature, setSignature] = React.useState(null);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const addSignaturePhoto = e => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append('custID', id);
+    form.append('photoOrSignature', 'P');
+    form.append('imageName', 'id');
+    form.append('imageFile', photo);
+    form.append('createdOn', '2021-05-18T08:24:25.200Z');
+    form.append('modifiedOn', '2021-05-18T08:24:25.200Z');
+    form.append('createdBy', 'BENVIK');
+    form.append('modifiedBy', 'BENVIK');
+    dispatch(SignaturePhotoAddition(form, history, id));
+  };
 
   return (
     <div className="individual-customer-form">
-      <Modal
-        modalText={modalText}
-        modalCloser={modalCloser}
-        openModel={openModel}
-      />
       <div className="lower-form-section">
         <div className="maintenance-customer-info">
           <span>Customer Signature and Photo Section</span>
@@ -26,15 +38,75 @@ const Signature = () => {
             <Sidebar />
           </div>
           <div className="submit-form-top-section">
-            <div className="signature-section">
-              <p>Signature section</p>
-            </div>
-            <MoreInfo modalOpener={modalOpener} />
+            <form className="main-form" onSubmit={addSignaturePhoto}>
+              <div className="signature-photo-section">
+                <div className="signature-section">
+                  <div className="uploaded-image">
+                    <div className="inner-image-section">
+                      {
+                        photo ? (
+                          <img
+                            src={URL.createObjectURL(photo)}
+                            alt={photo.name}
+                            style={{
+                              width: '90%',
+                              height: '90%',
+                            }}
+                          />
+                        ) : (<div className="photo-header">Photo</div>)
+                      }
+                    </div>
+                  </div>
+                  <div className="input-section">
+                    <div className="over-section-div">
+                      Upload Image
+                    </div>
+                    <div className="upload-input">
+                      <input
+                        type="file"
+                        onChange={e => setPhoto(e.target.files[0])}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="photo-section">
+                  <div className="uploaded-image">
+                    <div className="inner-image-section">
+                      {
+                        signature ? (
+                          <img
+                            src={URL.createObjectURL(signature)}
+                            alt={signature.name}
+                            style={{
+                              width: '90%',
+                              height: '90%',
+                            }}
+                          />
+                        ) : (<div className="photo-header">Signature</div>)
+                      }
+                    </div>
+                  </div>
+                  <div className="input-section">
+                    <div className="over-section-div">
+                      Upload Signature
+                    </div>
+                    <div className="upload-input">
+                      <input
+                        type="file"
+                        onChange={e => setSignature(e.target.files[0])}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="submit-button-section">
+                <button type="submit">Save</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
     </div>
-
   );
 };
 
