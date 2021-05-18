16d98b1ec025f0bc0ad6981b4befe3a52ec4c026
OdongAlican <sandieo.2020@gmail.com>
2021-05-19 01:07:50 +0300

    update layout

--- src/components/Pages/Indentification.jsx
+++ src/components/Pages/Indentification.jsx
@@ -1,40 +1,25 @@
 import React from 'react';
-import ModalFunction from '../Modal/ModalFunction';
-import Modal from '../Modal/Modal';
 import Sidebar from '../Sidebar/Sidebar';
-import MoreInfo from '../Customer/MoreInfo';
 import '../Customer/index.css';
 
-const Indentification = () => {
-  const {
-    modalCloser, modalOpener, openModel, modalText,
-  } = ModalFunction();
-
-  return (
-    <div className="individual-customer-form">
-      <Modal
-        modalText={modalText}
-        modalCloser={modalCloser}
-        openModel={openModel}
-      />
-      <div className="lower-form-section">
-        <div className="maintenance-customer-info">
-          <span>Customer Signature and Photo Section</span>
+const Indentification = () => (
+  <div className="individual-customer-form">
+    <div className="lower-form-section">
+      <div className="maintenance-customer-info">
+        <span>Customer Signature and Photo Section</span>
+      </div>
+      <div className="lower-downer-section">
+        <div className="left-inner-form-section">
+          <Sidebar />
         </div>
-        <div className="lower-downer-section">
-          <div className="left-inner-form-section">
-            <Sidebar />
-          </div>
-          <div className="submit-form-top-section">
-            <div className="signature-section">
-              <p>Identification section</p>
-            </div>
-            <MoreInfo modalOpener={modalOpener} />
+        <div className="submit-form-top-section">
+          <div className="signature-section">
+            <p>Identification section</p>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Indentification;
