f7ca38152dff4395f552428ae3d016fe509f916e
onenfelix <onenfelix90@gmail.com>
2021-05-19 13:30:32 +0300

    Added styles for system settings section, added also the dispatch function for retrieving company settings

--- src/components/System/SystemSettings/OtherSettings.jsx
+++ src/components/System/SystemSettings/OtherSettings.jsx
@@ -4,7 +4,7 @@
 /* eslint-disable react/no-array-index-key */
 
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import SetingsSidebar from './SettingsSideBar';
 import Modal from '../../Modal/Modal';
@@ -40,7 +40,160 @@ const SystemSettingsOtherSettings = () => {
           <div className="left-inner-form-section">
             <SetingsSidebar />
           </div>
-          <div className="submit-form-top-section"></div>
+          <div className="main-area">
+            <div className="left-column">
+              <div className="password-policy">
+                  <h4 class="text-center">Password Policy</h4>
+                  <hr />
+                  <form>
+                    <div class="form-item">
+                      <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Enforce Password History</label>
+                      <div class="form-input">
+                        <input type="number" class="form-input-input-select" />
+                      </div>
+                      <label for="name" class="col-form-label">Passwords Remembered <span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#63;</span> </label>
+                    </div>
+                    <div class="form-item">
+                      <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Maximum Password Age</label>
+                      <div class="form-input">
+                        <input type="number" class="form-input-input-select" />
+                      </div>
+                      <label for="name" class="col-form-label">Days <span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#63;</span> </label>
+                    </div>
+                    <div class="form-item">
+                      <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Minimum Password Length</label>
+                      <div class="form-input">
+                        <input type="number" class="form-input-input-select" />
+                      </div>
+                      <label for="name" class="col-form-label">Characters <span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#63;</span> </label>
+                    </div>
+                    <div class="form-item">
+                      <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Password Must Meet Complexity Requirements</label>
+                      <div class="form-input">
+                        <input type="checkbox" class="form-input-input-check" />
+                      </div>
+                      <label for="name" class="col-form-label">Disabled <span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#63;</span> </label>
+                    </div>
+                  </form>
+              </div>
+              <div className="other-setting">
+                  <h4 class="text-center">Other settings</h4>
+                  <hr />
+                  <form>
+                  <div class="form-item">
+                      <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Lock System After</label>
+                      <div class="form-input">
+                        <input type="number" class="form-input-input-select" />
+                      </div>
+                      <label for="name" class="col-form-label">Secs <span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#63;</span> </label>
+                    </div>
+                  </form>
+              </div>
+              <div className="buttons-action">
+                <NavLink to="/" style={{textDecoration: "none"}} className="buttons-link">Edit</NavLink>
+                <NavLink to="/" style={{textDecoration: "none"}} className="buttons-link">Save</NavLink>
+              </div>
+            </div>
+            <div className="right-column">
+                  <div className="available-balance">
+                    <h4>Available Balance Settings</h4>
+                    <hr />
+                    <form>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Clear Balance</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Lien Amount</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Frozen Amount</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Minimum Balance</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Unsupervised Credit</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Unsupervised Dedit</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                    </form>
+                  </div>
+                  <div className="total-balanace">
+                  <h4>Total Balance Settings</h4>
+                    <hr />
+                    <form>
+                    <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Clear Balance</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Lien Amount</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Frozen Amount</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Minimum Balance</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Unsupervised Credit</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                      <div class="form-item">
+                        <label for="name" class="col-form-label" style={{color:"#0178d4"}}>Unsupervised Dedit</label>
+                        <div class="form-input">
+                          <input type="checkbox" class="form-input-input-check" />
+                        </div>
+                        <label for="name" class="col-form-label"><span style={{display:"inline-block",padding:"3px",backgroundColor:"#D7D7D7"}}>&#43;</span> </label>
+                      </div>
+                    </form>
+                  </div>
+
+            </div>
+          </div>
 
         </div>
       </div>
