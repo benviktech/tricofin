87f7111b89aec2256c969f541e4c30fdebbb2b65
onenfelix <onenfelix90@gmail.com>
2021-05-19 13:31:26 +0300

    Added CompanyInfoReducer to the root reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
-import individualCustomersReducer from './individualCustomers';
-import staticDataReducer from './staticData';
+import { combineReducers } from "redux";
+import individualCustomersReducer from "./individualCustomers";
+import staticDataReducer from "./staticData";
+import companyInfoReducer from "./systemCompanyInfos";
 
 const rootReducer = combineReducers({
   individualCustomersReducer,
   staticDataReducer,
+  companyInfoReducer,
 });
 
 export default rootReducer;
