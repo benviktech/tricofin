0ce35bf6bfe0d773ad89f2ee34fb7faf65d24dae
OdongAlican <sandieo.2020@gmail.com>
2021-05-24 00:24:17 +0300

    add pages action

---
+++ src/actions/pages.js
@@ -0,0 +1,36 @@
+import {
+  PostIdentificationSuccessRequest,
+  GetIdentificationSuccessRequest,
+} from '../utils/api';
+
+export const POST_IDENTIFICATION_SUCCESS = 'POST_IDENTIFICATION_SUCCESS';
+export const LOADING_CONTENT = 'LOADING_CONTENT';
+export const LOADING_ERROR = 'LOADING_ERROR';
+
+export const individualCustomerIdentification = data => ({
+  type: POST_IDENTIFICATION_SUCCESS,
+  payload: data,
+});
+
+export const postCustomerIdentification = data => async dispatch => {
+  const method = 'post';
+  const path = '/api/Customers/SaveCustomerIdentification';
+  try {
+    dispatch({ type: LOADING_CONTENT });
+    const response = await PostIdentificationSuccessRequest(method, path, data);
+    dispatch(individualCustomerIdentification(response.data));
+  } catch (error) {
+    dispatch({ type: LOADING_ERROR, payload: error.message });
+  }
+};
+
+export const getCustomerIdentification = id => async dispatch => {
+  const method = 'get';
+  const path = `/api/Customers/GetCustomerIdentifications/${id}`;
+  try {
+    const response = await GetIdentificationSuccessRequest(method, path);
+    dispatch(individualCustomerIdentification(response.data));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
