7930d46ac3e8901c761864b8208439e5b9799a96
OdongAlican <sandieo.2020@gmail.com>
2021-05-25 21:43:57 +0300

    update methods

--- src/components/Pages/PrimaryContactInfo.jsx
+++ src/components/Pages/PrimaryContactInfo.jsx
@@ -1,221 +1,74 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import Sidebar from '../Sidebar/Sidebar';
-import '../Customer/index.css';
+/* eslint-disable no-restricted-globals */
 
-const ContactForm = () => (
-  <div className="individual-customer-form">
-    <div className="lower-form-section">
-      <div className="maintenance-customer-info">
-        <span>Primary Contact Info</span>
-      </div>
-      <div className="lower-downer-section">
-        <div className="left-inner-form-section">
-          <Sidebar />
-        </div>
-        <div className="submit-form-top-section">
-          <form className="contact-info-section">
-            <div className="left-address-section">
-              <div className="building-name">
-                <div className="label-unit">Building Name:</div>
-                <div className="building-input">
-                  <input type="text" />
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit">
-                  Unit No:
-                </div>
-                <div className="unit-section">
-                  <input type="text" />
-                  <div className="floor-section">
-                    <div className="inner-label-unit">
-                      Floor No:
-                    </div>
-                    <input type="text" />
-                  </div>
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit">
-                  LC/Street:
-                </div>
-                <div className="unit-section">
-                  <input type="text" />
-                  <div className="floor-section">
-                    <div className="inner-label-unit">
-                      Plot/Street No:
-                    </div>
-                    <input type="text" />
-                  </div>
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit">
-                  Parish:
-                </div>
-                <div className="unit-section select-area">
-                  <select>
-                    <option>select</option>
-                    <option>first</option>
-                    <option>second</option>
-                  </select>
-                  <div className="floor-section">
-                    <div className="inner-label-unit">
-                      Suburb:
-                    </div>
-                    <select>
-                      <option>select</option>
-                      <option>first</option>
-                      <option>second</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit">
-                  Village:
-                </div>
-                <div className="unit-section select-area">
-                  <select>
-                    <option>select</option>
-                    <option>first</option>
-                    <option>second</option>
-                  </select>
-                  <div className="floor-section">
-                    <div className="inner-label-unit">
-                      County/Town:
-                    </div>
-                    <select>
-                      <option>select</option>
-                      <option>first</option>
-                      <option>second</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <div className="building-name">
-                <div className="label-unit">District:</div>
-                <div className="building-input">
-                  <input type="text" />
-                </div>
-              </div>
-              <div className="building-name">
-                <div className="label-unit">Region:</div>
-                <div className="building-input">
-                  <select>
-                    <option>select</option>
-                    <option>first</option>
-                    <option>second</option>
-                  </select>
-                </div>
-              </div>
-              <div className="building-name">
-                <div className="label-unit">Country:</div>
-                <div className="building-input">
-                  <select>
-                    <option>select</option>
-                    <option>first</option>
-                    <option>second</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-            <div className="right-address-section">
-              <div className="unit-floor-section">
-                <div className="label-unit unit-left-section">
-                  P.O Box No:
-                </div>
-                <div className="unit-section unit-right-section">
-                  <input type="text" />
-                  <div className="floor-section lower-floor-section">
-                    <div className="inner-label-unit">
-                      Post Office Town:
-                    </div>
-                    <select>
-                      <option>select</option>
-                      <option>first</option>
-                      <option>second</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit unit-left-section">
-                  Been At Address Since:
-                </div>
-                <div className="unit-section unit-right-section">
-                  <div className="lower-floor-section">
-                    <input type="date" />
-                  </div>
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit unit-left-section">
-                  Primary Tel No:
-                </div>
-                <div className="unit-section unit-right-section">
-                  <input type="text" />
-                  <div className="floor-section lower-floor-section">
-                    <div className="inner-label-unit">
-                      Other No:
-                    </div>
-                    <input type="text" />
-                  </div>
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit unit-left-section">
-                  Mobile No:
-                </div>
-                <div className="unit-section unit-right-section">
-                  <input type="text" />
-                  <div className="floor-section lower-floor-section">
-                    <div className="inner-label-unit">
-                      Fax No:
-                    </div>
-                    <input type="text" />
-                  </div>
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit unit-left-section">
-                  Email Address:
-                </div>
-                <div className="unit-section unit-right-section">
-                  <div className="lower-floor-section ">
-                    <input type="input" />
-                  </div>
-                </div>
-              </div>
-              <div className="unit-floor-section">
-                <div className="label-unit unit-left-section">
-                  Website:
-                </div>
-                <div className="unit-section unit-right-section">
-                  <div className="lower-floor-section ">
-                    <input type="input" />
-                  </div>
-                </div>
-              </div>
-              <div className="add-form-data-button">
-                <button
-                  type="submit"
-                >
-                  Add
-                </button>
-                <button
-                  type="button"
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router';
+import ContactInfo from './ContactInfo';
+import { postCustomerContact } from '../../actions/pages';
+import contactValidator from '../Validators/ContactValidator';
 
-export default ContactForm;
+const PrimaryContactForm = () => {
+  const headerPrimaryContact = 'Primary Contact Information';
+  const [errors, setErrors] = useState({});
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const [values, setValues] = useState(
+    {
+      custID: id,
+      contactType: 'P',
+      buildingName: '',
+      floorNo: '',
+      plotStreetNo: '',
+      lcStreetName: '',
+      parish: '',
+      suburb: '',
+      village: '',
+      countyTown: '',
+      district: '',
+      region: '',
+      poBoxNo: '',
+      postOfficeTown: '',
+      countryCode: '',
+      atAddressSince: '',
+      pnTelNo: '',
+      mobNo: '',
+      emailAddress: '',
+      website: '',
+      createdBy: 'BENVIK',
+      createdOn: '2021-05-25T11:48:42.653Z',
+      modifiedBy: 'BENVIK',
+      modifiedOn: '2021-05-25T11:48:42.653Z',
+    },
+  );
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const submitPrimaryContact = e => {
+    e.preventDefault();
+    setErrors(contactValidator(values));
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0) {
+      dispatch(postCustomerContact(values));
+    }
+  }, [errors]);
+
+  return (
+    <ContactInfo
+      header={headerPrimaryContact}
+      submitForm={submitPrimaryContact}
+      handleChange={handleChange}
+      values={values}
+    />
+  );
+};
+
+export default PrimaryContactForm;
