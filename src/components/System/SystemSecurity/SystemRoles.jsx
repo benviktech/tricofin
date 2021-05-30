1bbed54f4b66a46750dd9c7d6420e36d56865a63
onenfelix <onenfelix90@gmail.com>
2021-05-30 15:31:55 +0300

    Created form for adding roles, table for showing roles, and nested component for displaying users of that role

--- src/components/System/SystemSecurity/SystemRoles.jsx
+++ src/components/System/SystemSecurity/SystemRoles.jsx
@@ -4,7 +4,7 @@
 /* eslint-disable react/no-array-index-key */
 
 import React, { useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useRouteMatch, Switch, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import SecuritySidebar from './SecuritySideBar';
 import Modal from '../../Modal/Modal';
@@ -12,15 +12,32 @@ import ModalFunction from '../../Modal/ModalFunction';
 import './index.css';
 import Loader from '../../Loader/Loader';
 import { fetchCompanyInfo } from "../../../actions/systemCompanyInfo"
+import RoleMembers from './RoleMembers';
+import {Button} from '../../_generics/Generics';
 
 const SystemSecurityRoles = () => {
   const dispatch = useDispatch();
+  const {path, url} = useRouteMatch();
+  const btnName = 'Add';
 
   const {
     modalCloser, modalOpener, openModel, modalText,
   } = ModalFunction();
 
-
+  const rolesList = [
+    {
+      id: "ADMIN",
+      desc: "Aministrator",
+    },
+    {
+      id: "CASHIER",
+      desc: "Cashier",
+    },
+    {
+      id: "USER",
+      desc: "User",
+    },
+  ]
 
   const companyInfo = useSelector(state => state.companyInfoReducer);
   console.log(companyInfo)
@@ -46,10 +63,64 @@ const SystemSecurityRoles = () => {
             <SecuritySidebar />
           </div>
           <div className="submit-form-top-section">
+            <div className="create-role-section">
+              <form>
+                <div className="role-id w-50 d-flex justify-content-end">
+                  <label htmlFor="role-id"> Role Id:
+                    <input className="ml-2" type="text" />
+                  </label>
+                </div>
+                <div className="description w-50 d-flex justify-content-end">
+                  <label htmlFor="description"> Description:
+                  <input className="ml-2" type="text" />
+                  </label>
+                </div>
+              </form>
+            </div>
+            <div className="roles-lower-form-section">
+              <div className="display-roles-section bg-warning">
+                <div className="user-role-listings">
+                  User Role Listing
+                </div>
+                <div className="roles-header-section">
+                  <div className="column-one">
 
+                  </div>
+                  <div className="column-two">Role ID</div>
+                  <div className="column-three">Description</div>
+                </div>
+                  {
+                    rolesList.map(role => (
+                      <div
+                      className="roles-header-section">
+                      <div className="column-one">
+    
+                      </div>
+                      <Link
+                      to={{
+                        pathname: `${url}/${role.id}`
+                      }}  className="column-two">{role.id}</Link>
+                      <Link
+                      to={{
+                        pathname: `${url}/${role.id}`
+                      }}  className="column-three">{role.desc}</Link>
+                    </div>
+                    ))
+                  }
+              </div>
+              <div className="display-users-section">
+                <Switch>
+                  <Route 
+                    path={`${path}/:id`} 
+                    component={RoleMembers}></Route>
+                </Switch>
+              </div>
           </div>
-
-
+          <div className="cancel-submit-buttons-section">
+                <Button name="Add"/>
+                <Button name="Cancel"/>
+              </div>
+            </div>
         </div>
       </div>
     </div>
