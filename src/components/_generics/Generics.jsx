cb4f986e8410d8d740681cf2a794e39fe6e12886
onenfelix <onenfelix90@gmail.com>
2021-06-03 09:58:43 +0300

    Modified inputs and buttons to accept more props, and added styling for them

--- src/components/_generics/Generics.jsx
+++ src/components/_generics/Generics.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './index.css';
 
-export const Button = ({name}) => (
-    <button>{name}</button>
+export const Button = ({name, onClick, type="button"}) => (
+    <button
+    type={type}
+    onClick={onClick}
+    >{name}</button>
 );
 
-export const Input = ({type}) => (
-    <input type={type} />
+export const Input = ({type, handleChange, value, name, placeholder, disabled,checked}) => (
+    <input
+    onChange={handleChange}
+    type={type}
+    name={name}
+    value= {value} 
+    placeholder={placeholder}
+    disabled={disabled}
+    checked={checked}
+    />
 );
