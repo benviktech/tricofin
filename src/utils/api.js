34ef1271ef5876f438176b63e4a7021bae0fe413
onenfelix <onenfelix90@gmail.com>
2021-06-04 01:53:00 +0300

    Added methods for fetching system modules, updating company info, fetching system roles, posting system roles, posting system user, updating system user, and deleting system user

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://tricoapi.azurewebsites.net";
+const baseUrl = "https://tricofin.azurewebsites.net";
 
 export const FetchIndividualCustomersRequest = async (method, path) => {
   const response = await axios[method](`${baseUrl}/${path}`);
@@ -33,6 +33,41 @@ export const FetchStaticData = async (method, path) => {
 };
 
 export const FetchCompanyInfoRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
+
+export const FetchSystemModuleRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
+
+export const UpdateCompanyInfoRequest = async (method, result, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`, result);
+  return response;
+};
+
+export const FetchSystemRoleRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
+
+export const PostSystemsRoleRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
+
+export const PostSystemUserRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
+
+export const UpdateSystemUserRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
+
+export const DeleteSystemUserRequest = async (method, path) => {
   const response = await axios[method](`${baseUrl}/${path}`);
   return response;
 };
