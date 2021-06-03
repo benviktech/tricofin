048b09e51d11e621ea0b05b90531ea2e6ab38958
onenfelix <onenfelix90@gmail.com>
2021-06-03 09:38:00 +0300

    Created the form for creating roles and table for displaying available system roles

--- src/components/System/SystemSecurity/SystemRoles.jsx
+++ src/components/System/SystemSecurity/SystemRoles.jsx
@@ -3,51 +3,82 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/no-array-index-key */
 
-import React, { useEffect } from 'react';
-import { Link, NavLink, useRouteMatch, Switch, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useRouteMatch, Switch, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import SecuritySidebar from './SecuritySideBar';
 import Modal from '../../Modal/Modal';
 import ModalFunction from '../../Modal/ModalFunction';
 import './index.css';
 import Loader from '../../Loader/Loader';
-import { fetchCompanyInfo } from "../../../actions/systemCompanyInfo"
 import RoleMembers from './RoleMembers';
-import {Button} from '../../_generics/Generics';
+import {Button, Input} from '../../_generics/Generics';
+import { createSystemRole, fetchSystemRoles } from '../../../actions/systemRole';
 
 const SystemSecurityRoles = () => {
   const dispatch = useDispatch();
+  const initialState = {
+    roleID: "",
+    description: "",
+    createdOn: "2021-05-30T15:02:01.437Z",
+    createdBy: "BENVIK",
+    modifiedOn: "2021-05-30T15:02:01.437Z",
+    modifiedBy: "BENVIK"
+  }
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
   const {path, url} = useRouteMatch();
-  const btnName = 'Add';
-
   const {
-    modalCloser, modalOpener, openModel, modalText,
+    modalCloser, openModel, modalText,
   } = ModalFunction();
 
-  const rolesList = [
-    {
-      id: "ADMIN",
-      desc: "Aministrator",
-    },
-    {
-      id: "CASHIER",
-      desc: "Cashier",
-    },
-    {
-      id: "USER",
-      desc: "User",
-    },
-  ]
-
+  const roles = useSelector(state => state.systemRoleReducer);
   const companyInfo = useSelector(state => state.companyInfoReducer);
-  console.log(companyInfo)
-
+  
   useEffect(() => {
-    dispatch(fetchCompanyInfo());
+    dispatch(fetchSystemRoles());
   }, []);
 
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  }
 
-  return companyInfo ? (
+  const submitRole = () => {
+    const result = {}
+    if(!values.description.trim('')){
+      result.description = 'Description is required'
+    }
+    if(!values.roleID.trim('')){
+      result.roleID = 'Role ID is required'
+    }
+    if(values.roleID.length > 4){
+      result.roleID = 'Max Length is 4 Characters'
+    }
+
+    setErrors(result);
+  }
+
+  useEffect(() => {
+    if(Object.keys(errors).length === 0){
+      console.log(values);
+      dispatch(createSystemRole(values));
+    }
+  }, [errors])
+
+  const clearFields = () => {
+    console.log('clear fields')
+  }
+
+  return roles.loading ? (
+    <div>
+      <Loader />
+      <h1>{roles.error}</h1>
+    </div>
+  ) : roles.systemRoles.length > 0 ? (
     <div className="individual-customer-form">
       <Modal
         modalText={modalText}
@@ -65,21 +96,29 @@ const SystemSecurityRoles = () => {
           <div className="submit-form-top-section">
             <div className="create-role-section">
               <form>
-                <div className="role-id w-50 d-flex justify-content-end">
+                <div className="role-id d-flex">
                   <label htmlFor="role-id"> Role Id:
-                    <input className="ml-2" type="text" />
                   </label>
+                    <Input 
+                    name = "roleID"
+                    handleChange = {handleChange}
+                    value = {values.roleID}
+                    type="text" />
                 </div>
-                <div className="description w-50 d-flex justify-content-end">
+                <div className="description my-2 d-flex">
                   <label htmlFor="description"> Description:
-                  <input className="ml-2" type="text" />
                   </label>
+                    <Input 
+                    name = "description"
+                    value={values.description}
+                    handleChange = {handleChange}
+                    type="text" />
                 </div>
               </form>
             </div>
             <div className="roles-lower-form-section">
-              <div className="display-roles-section bg-warning">
-                <div className="user-role-listings">
+              <div className="display-roles-section">
+                <div className="user-role-listings p-2">
                   User Role Listing
                 </div>
                 <div className="roles-header-section">
@@ -90,20 +129,19 @@ const SystemSecurityRoles = () => {
                   <div className="column-three">Description</div>
                 </div>
                   {
-                    rolesList.map(role => (
+                    roles.systemRoles.map(role => (
                       <div
                       className="roles-header-section">
                       <div className="column-one">
-    
                       </div>
                       <Link
                       to={{
-                        pathname: `${url}/${role.id}`
-                      }}  className="column-two">{role.id}</Link>
+                        pathname: `${url}/${role.roleID}`
+                      }}  className="column-two">{role.roleID}</Link>
                       <Link
                       to={{
-                        pathname: `${url}/${role.id}`
-                      }}  className="column-three">{role.desc}</Link>
+                        pathname: `${url}/${role.roleID}`
+                      }}  className="column-three">{role.description}</Link>
                     </div>
                     ))
                   }
@@ -117,8 +155,12 @@ const SystemSecurityRoles = () => {
               </div>
           </div>
           <div className="cancel-submit-buttons-section">
-                <Button name="Add"/>
-                <Button name="Cancel"/>
+                <Button 
+                onClick = {submitRole}
+                name="Add"/>
+                <Button 
+                onClick = {clearFields}
+                name="Cancel"/>
               </div>
             </div>
         </div>
@@ -126,7 +168,7 @@ const SystemSecurityRoles = () => {
     </div>
   ) : (
     <div className="spinner section">
-      <Loader />
+      {roles.error}
     </div>
   );
 };
