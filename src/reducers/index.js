0e1397e16447d6ffe737d420e6c59b3c6ddec760
onenfelix <onenfelix90@gmail.com>
2021-06-04 01:50:23 +0300

    Added system module, system role and system user reducer to the root reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,17 @@ import { combineReducers } from "redux";
 import individualCustomersReducer from "./individualCustomers";
 import staticDataReducer from "./staticData";
 import companyInfoReducer from "./systemCompanyInfos";
+import systemModuleReducer from "./systemRights";
+import systemRoleReducer from "./systemRoles";
+import systemUserReducer from "./systemUsers";
 
 const rootReducer = combineReducers({
   individualCustomersReducer,
   staticDataReducer,
   companyInfoReducer,
+  systemModuleReducer,
+  systemRoleReducer,
+  systemUserReducer,
 });
 
 export default rootReducer;
