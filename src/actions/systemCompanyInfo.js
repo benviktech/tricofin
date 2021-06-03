5c521fff26f1f7f46361dca219e38f5adf176089
onenfelix <onenfelix90@gmail.com>
2021-06-03 09:43:10 +0300

    Added action methods for retrieving company settings and updating settings

--- src/actions/systemCompanyInfo.js
+++ src/actions/systemCompanyInfo.js
@@ -1,4 +1,7 @@
-import { FetchCompanyInfoRequest } from "../utils/api";
+import {
+  FetchCompanyInfoRequest,
+  UpdateCompanyInfoRequest,
+} from "../utils/api";
 
 export const FETCH_COMPANY_INFO_SUCCESS = "FETCH_COMPANY_INFO_SUCCESS";
 export const POST_COMPANY_INFO_SUCCESS = "POST_COMPANY_INFO_SUCCESS";
@@ -17,11 +20,45 @@ export const companyInfoSuccessPost = (data) => ({
 
 export const fetchCompanyInfo = () => async (dispatch) => {
   const method = "get";
-  const path = "api/System/GetRoles";
+  const path = "/api/System/GetSystemSettings";
   try {
     dispatch({ type: LOADING_CONTENT });
     const response = await FetchCompanyInfoRequest(method, path);
     dispatch(companyInfoSuccessFetch(response.data));
+  } catch (error) {
+    dispatch({ type: LOADING_ERROR, payload: error.message });
+  }
+};
+
+export const updateCompanyInfo = (data, history) => async (dispatch) => {
+  const method = "put";
+  const path = "api/System/UpdateSystemSettings";
+  const result = {
+    companyID: data.companyID,
+    companyName: data.companyName,
+    pAddress: data.pAddress,
+    boxAddress: data.boxAddress,
+    email: data.email,
+    phone: data.phone,
+    website: data.website,
+    pswdHistory: parseInt(data.pswdHistory, 10),
+    pswdAge: parseInt(data.pswdAge, 10),
+    pswdLength: parseInt(data.pswdLength, 10),
+    lockSysPeriod: parseInt(data.lockSysPeriod, 10),
+    availableBalance: data.availableBalance,
+    totalBalance: data.availableBalance,
+    createdOn: data.createdOn,
+    createdBy: data.createdBy,
+    modifiedOn: data.modifiedOn,
+    modifiedBy: data.modifiedBy,
+  };
+
+  console.log(result);
+
+  try {
+    dispatch({ type: LOADING_CONTENT });
+    const response = await UpdateCompanyInfoRequest(method, result, path);
+    history.push(`/systemsettingscompanyinfo`);
   } catch (error) {
     dispatch({ type: LOADING_ERROR, payload: error.message });
   }
