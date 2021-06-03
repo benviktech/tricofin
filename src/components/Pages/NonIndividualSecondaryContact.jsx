05d1f7d7617c527431931d6f6389cb7e9c720467
OdongAlican <sandieo.2020@gmail.com>
2021-06-03 22:43:21 +0300

    add Secondary contact component

---
+++ src/components/Pages/NonIndividualSecondaryContact.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ModalFunction from '../Modal/ModalFunction';
+import Modal from '../Modal/Modal';
+import { NonIdividualSidebar } from '../Sidebar/Sidebar';
+import MoreInfo from '../NonIdividual/MoreInfo';
+
+const NonIndividualSecondaryContact = () => {
+  const {
+    modalCloser, modalOpener, openModel, modalText,
+  } = ModalFunction();
+  return (
+    <div className="individual-customer-form">
+      <Modal
+        modalText={modalText}
+        modalCloser={modalCloser}
+        openModel={openModel}
+      />
+      <div className="lower-form-section">
+        <div className="maintenance-customer-info">
+          <span>Non Individual Customer Information</span>
+        </div>
+        <div className="lower-downer-section">
+          <div className="left-inner-form-section">
+            <NonIdividualSidebar />
+          </div>
+          <div className="submit-form-top-section">
+            <p>Secondary contact section</p>
+            <MoreInfo modalOpener={modalOpener} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NonIndividualSecondaryContact;
