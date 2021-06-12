470fb0f345abde2900edd88fa4b8fd03b06ff628
onenfelix <onenfelix90@gmail.com>
2021-06-13 01:42:18 +0300

    Added maxlength attribute to the input generics, set default uppercase styling for inputs

--- src/components/_generics/Generics.jsx
+++ src/components/_generics/Generics.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
-export const Button = ({name, onClick, type="button"}) => (
-    <button
-    type={type}
-    onClick={onClick}
-    >{name}</button>
+export const Button = ({ name, onClick, type = "button" }) => (
+  <button type={type} onClick={onClick}>
+    {name}
+  </button>
 );
 
-export const Input = ({type, handleChange, value, name, placeholder, disabled,checked}) => (
-    <input
+export const Input = ({
+  type,
+  handleChange,
+  value,
+  name,
+  placeholder,
+  disabled,
+  checked,
+  maxlength,
+}) => (
+  <input
     onChange={handleChange}
     type={type}
     name={name}
-    value= {value} 
+    value={value}
     placeholder={placeholder}
     disabled={disabled}
     checked={checked}
-    />
+    maxlength={maxlength}
+    style={{ textTransform: "uppercase" }}
+  />
 );
