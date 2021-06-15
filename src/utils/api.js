86a41e97faf3c8a3379f770d6add918ac613fc0e
onenfelix <onenfelix90@gmail.com>
2021-06-15 14:25:50 +0300

    fixed problems in the api.js file

--- src/utils/api.js
+++ src/utils/api.js
@@ -135,3 +135,38 @@ export const FetchCompanyInfoRequest = async (method, path) => {
   const response = await axios[method](`${baseUrl}/${path}`);
   return response;
 };
+
+export const FetchCompanyInfoRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
+
+export const FetchSystemModuleRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
+
+export const UpdateCompanyInfoRequest = async (method, result, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`, result);
+  return response;
+};
+
+export const FetchSystemRoleRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
+
+export const PostSystemsRoleRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
+
+export const PostSystemUserRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
+
+export const UpdateSystemUserRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
