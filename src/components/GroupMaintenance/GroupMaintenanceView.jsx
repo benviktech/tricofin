3b7b41b050ee0096ed8eef18f0427ff39fd7f0fb
OdongAlican <sandieo.2020@gmail.com>
2021-06-16 19:16:51 +0300

    update group maintenance view component

--- src/components/GroupMaintenance/GroupMaintenanceView.jsx
+++ src/components/GroupMaintenance/GroupMaintenanceView.jsx
@@ -1,181 +1,243 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
+import { getGroupMaintenance } from '../../actions/groupMaintenance';
 import { GroupMaintenanceSidebar } from '../Sidebar/Sidebar';
 import './index.css';
 import MoreInfo from './MoreInfo';
+import meetingDays from './FormData';
 
-const GroupMaintenanceView = () => (
-  <div className="individual-customer-form">
-    <div className="lower-form-section">
-      <div className="maintenance-customer-info">
-        <span>Customer Information</span>
-      </div>
-      <div className="lower-downer-section">
-        <div className="left-inner-form-section">
-          <GroupMaintenanceSidebar />
+const GroupMaintenanceView = () => {
+  const { id } = useParams();
+  const [frequencies, setFrequencies] = useState([]);
+  const dispatch = useDispatch();
+
+  const groupDetails = useSelector(state => state.groupMaintenanceReducer);
+
+  useEffect(() => {
+    dispatch(getGroupMaintenance(id));
+  }, []);
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get('https://tricofin.azurewebsites.net/api/StaticData/GetSystemFrequencies')
+        .then(response => setFrequencies(response.data))
+        .catch(error => console.log(error.message));
+    };
+
+    fetchData();
+  }, []);
+
+  const sortMeetingDay = day => {
+    let date;
+    meetingDays.forEach(meet => {
+      if (meet.value === day) {
+        date = meet.day;
+      }
+    });
+    return date;
+  };
+
+  const sortMeetingFrequency = freq => {
+    let result;
+    if (frequencies.length > 0) {
+      frequencies.forEach(frequency => {
+        if (frequency.freqID === freq) {
+          result = frequency.frequency;
+        }
+      });
+    }
+
+    return result;
+  };
+
+  return (
+    <div className="individual-customer-form">
+      <div className="lower-form-section">
+        <div className="maintenance-customer-info">
+          <span>Customer Information</span>
         </div>
-        <div className="submit-form-top-section">
-          <div className="main-form-color boder">
-            <div className="left-section">
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Search Group Name:</div>
-                <div className="right-horizontal-section">
-                  <div className="information-section">
-                    information
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Formation Date:</div>
-                <div className="right-horizontal-section">
-                  <div className="inner-left-section">
-                    <div className="information-section">
-                      information
-                    </div>
-                  </div>
-                  <div className="inner-right-section">
-                    <div className="inner-right-label">Reg Date :</div>
-                    <div className="inner-right-input">
-                      <div className="information-section">
-                        information
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Location :</div>
-                <div className="right-horizontal-section">
-                  <div className="inner-left-section">
-                    <div className="information-section">
-                      information
-                    </div>
-                  </div>
-                  <div className="inner-right-section">
-                    <div className="inner-right-label">Reg No :</div>
-                    <div className="inner-right-input">
-                      <div className="information-section">
-                        information
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Village :</div>
-                <div className="right-horizontal-section">
-                  <div className="information-section">
-                    information
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Sourced By :</div>
-                <div className="right-horizontal-section">
-                  <div className="information-section-left">
-                    information
-                  </div>
-                  <div className="information-section">
-                    information
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Credit Officer :</div>
-                <div className="right-horizontal-section">
-                  <div className="information-section-left">
-                    information
-                  </div>
-                  <div className="information-section">
-                    information
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="right-section">
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Savings Product :</div>
-                <div className="right-horizontal-section">
-                  <div className="information-section-left">
-                    information
-                  </div>
-                  <div className="information-section">
-                    information
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Loan Product :</div>
-                <div className="right-horizontal-section">
-                  <div className="information-section-left">
-                    information
-                  </div>
-                  <div className="information-section">
-                    information
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Max Members Allowed :</div>
-                <div className="right-horizontal-section">
-                  <div className="inner-left-section">
-                    <div className="information-section">
-                      information
-                    </div>
-                  </div>
-                  <div className="inner-right-section">
-                    <div className="inner-right-label members-number">
-                      Min Members for Loan Disburesment
-                    </div>
-                    <div className="inner-right-input">
-                      <div className="information-section">
-                        information
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Meeting Day :</div>
-                <div className="right-horizontal-section">
-                  <div className="inner-left-section select-section">
-                    <div className="information-section">
-                      information
-                    </div>
-                  </div>
-                  <div className="inner-right-section select-section-two">
-                    <div className="inner-right-label">Meeting Frequency :</div>
-                    <div className="inner-right-input">
-                      <div className="information-section">
-                        information
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="horizontal-section">
-                <div className="left-horizontal-section">Meeting Place :</div>
-                <div className="right-horizontal-section">
-                  <div className="information-section">
-                    information
-                  </div>
-                </div>
-              </div>
-
-              <div className="submit-button-section">
-                <button
-                  type="button"
-                  className="add-customer-btn"
-                >
-                  Update
-                </button>
-              </div>
-            </div>
+        <div className="lower-downer-section">
+          <div className="left-inner-form-section">
+            <GroupMaintenanceSidebar />
           </div>
-          <MoreInfo />
+          <div className="submit-form-top-section">
+            <div className="main-form-color boder">
+              <div className="left-section">
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Group Name:</div>
+                  <div className="right-horizontal-section">
+                    <div className="information-section">
+                      {groupDetails.groupMaintenance.groupName}
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Formation Date:</div>
+                  <div className="right-horizontal-section">
+                    <div className="inner-left-section">
+                      <div className="information-section">
+                        {new Date(groupDetails.groupMaintenance.formationDate)
+                          .toUTCString()
+                          .split(' ')
+                          .slice(0, 4)
+                          .join(' ')}
+                      </div>
+                    </div>
+                    <div className="inner-right-section">
+                      <div className="inner-right-label">Reg Date :</div>
+                      <div className="inner-right-input">
+                        <div className="information-section">
+                          {new Date(groupDetails.groupMaintenance.regDate)
+                            .toUTCString()
+                            .split(' ')
+                            .slice(0, 4)
+                            .join(' ')}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Location :</div>
+                  <div className="right-horizontal-section">
+                    <div className="inner-left-section">
+                      <div className="information-section">
+                        {groupDetails.groupMaintenance.location}
+                      </div>
+                    </div>
+                    <div className="inner-right-section">
+                      <div className="inner-right-label">Reg No :</div>
+                      <div className="inner-right-input">
+                        <div className="information-section">
+                          {groupDetails.groupMaintenance.regNo
+                          && (groupDetails.groupMaintenance.regNo).toUpperCase()}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Village :</div>
+                  <div className="right-horizontal-section">
+                    <div className="information-section">
+                      {groupDetails.groupMaintenance.village}
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Sourced By :</div>
+                  <div className="right-horizontal-section">
+                    <div className="information-section-left">
+                      information
+                    </div>
+                    <div className="information-section">
+                      {groupDetails.groupMaintenance.sourcedBy}
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Credit Officer :</div>
+                  <div className="right-horizontal-section">
+                    <div className="information-section-left">
+                      information
+                    </div>
+                    <div className="information-section">
+                      {groupDetails.groupMaintenance.creditOfficer}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="right-section">
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Savings Product :</div>
+                  <div className="right-horizontal-section">
+                    <div className="information-section-left">
+                      information
+                    </div>
+                    <div className="information-section">
+                      {groupDetails.groupMaintenance.savingsProductID}
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Loan Product :</div>
+                  <div className="right-horizontal-section">
+                    <div className="information-section-left">
+                      information
+                    </div>
+                    <div className="information-section">
+                      {groupDetails.groupMaintenance.savingsProductID}
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Max Members Allowed :</div>
+                  <div className="right-horizontal-section">
+                    <div className="inner-left-section">
+                      <div className="information-section">
+                        {groupDetails.groupMaintenance.maxMembers}
+                      </div>
+                    </div>
+                    <div className="inner-right-section">
+                      <div className="inner-right-label members-number">
+                        Min Members for Loan Disburesment
+                      </div>
+                      <div className="inner-right-input">
+                        <div className="information-section">
+                          {groupDetails.groupMaintenance.minMembersLoanDisb}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Meeting Day :</div>
+                  <div className="right-horizontal-section">
+                    <div className="inner-left-section select-section">
+                      <div className="information-section">
+                        {sortMeetingDay(groupDetails.groupMaintenance.meetingDay)}
+                      </div>
+                    </div>
+                    <div className="inner-right-section select-section-two">
+                      <div className="inner-right-label">Meeting Frequency :</div>
+                      <div className="inner-right-input">
+                        <div className="information-section">
+                          {sortMeetingFrequency(groupDetails.groupMaintenance.meetingFreq)}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="horizontal-section">
+                  <div className="left-horizontal-section">Meeting Place :</div>
+                  <div className="right-horizontal-section">
+                    <div className="information-section">
+                      {groupDetails.groupMaintenance.meetingPlace}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="submit-button-section">
+                  <Link
+                    className="add-customer-btn"
+                    to={{
+                      pathname: `/updategroupmember/${groupDetails.groupMaintenance.groupID}`,
+                    }}
+                  >
+                    Update
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <MoreInfo />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default GroupMaintenanceView;
