2d7f3984d3695e2acdb77a955aa416c8ffb03064
OdongAlican <sandieo.2020@gmail.com>
2021-06-16 19:19:09 +0300

    update api requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -101,3 +101,18 @@ export const DeleteCustomerDirector = async (method, path) => {
   const response = await axios[method](`${baseUrl}/${path}`);
   return response;
 };
+
+export const PostGroupMaintenanceRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
+
+export const GetGroupMaintenanceRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
+
+export const UpdateGroupMaintenanceRequest = async (method, data, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
