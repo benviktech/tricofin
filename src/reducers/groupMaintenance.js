5dac4a57e7edfe7b41aa8c1aadf6b7a5b7cd741f
OdongAlican <sandieo.2020@gmail.com>
2021-06-25 16:39:57 +0300

    add ListOfMembers to reducer

--- src/reducers/groupMaintenance.js
+++ src/reducers/groupMaintenance.js
@@ -2,12 +2,14 @@ import {
   POST_GROUP_MAINTENANCE_SUCCESS,
   LOADING_CONTENT,
   LOADING_ERROR,
+  GROUP_MAINTENANCE_LIST,
 } from '../actions/groupMaintenance';
 
 const initialState = {
   error: '',
   loading: false,
   groupMaintenance: {},
+  listOfMembers: [],
 };
 
 const groupMaintenanceReducer = (state = initialState, action) => {
@@ -30,6 +32,13 @@ const groupMaintenanceReducer = (state = initialState, action) => {
         ...state,
         loading: false,
         error: action.payload,
+      };
+    case GROUP_MAINTENANCE_LIST:
+      return {
+        ...state,
+        loading: false,
+        error: '',
+        listOfMembers: action.payload,
       };
     default:
       return state;
