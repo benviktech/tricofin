5ee20e9650ea9d6990939751630847904a51c649
onenfelix <onenfelix90@gmail.com>
2021-06-30 21:02:08 +0300

    Finalized the section for setting a holiday date or a range of holiday date, added validation, included requests to API

--- src/components/System/Holidays/SystemHolidays.jsx
+++ src/components/System/Holidays/SystemHolidays.jsx
@@ -10,7 +10,7 @@ import HolidaySidebar from "./HolidaySideBar";
 import "./index.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Loader from "../../Loader/Loader";
+import Loader from "./Loader/Loader";
 import { Button, Input } from "../../_generics/Generics";
 import Spinner from "../../Spinner/Spinner";
 import axios from "axios";
@@ -25,6 +25,17 @@ const SystemHolidays = () => {
   const [endDate, setEndDate] = useState(null);
   const [holidays, setHolidays] = useState([]);
   const [workingDates, setWorkingDates] = useState([]);
+  // const [remarks, setRemarks] = useState("");
+  const [errors, setErrors] = useState({});
+  const initialHolidate = {
+    holidayDate: startDate,
+    remarks: "",
+    createdOn: "2021-06-30T15:06:28.108Z",
+    modifiedBy: "BENEVIK",
+    createdBy: "BENEVIK",
+    modifiedOn: "2021-06-30T15:06:28.108Z",
+  };
+  const [holidate, setHolidate] = useState(initialHolidate);
 
   const fetchSystemHolidays = async () => {
     axios
@@ -35,10 +46,28 @@ const SystemHolidays = () => {
       .catch(function (error) {});
   };
 
+  const createSystemHolidays = async () => {
+    const response = HolidayValidator(workingDates);
+    setErrors(response);
+    if (Object.keys(response).length === 0) {
+      console.log(workingDates);
+      axios
+        .post(`${baseUrl}/api/System/SaveSystemHolidays`, workingDates)
+        .then(function (response) {
+          setHolidays(response.data);
+          toast.success(`Holiday set Successfully`);
+        })
+        .catch(function (error) {
+          toast.error(`Failed to create holiday`);
+        });
+    }
+  };
+
   const clearDate = () => {
     setWorkingDates([]);
     setStartDate("");
     setEndDate(null);
+    setHolidate(initialHolidate);
   };
 
   Date.prototype.addDays = function (days) {
@@ -48,10 +77,10 @@ const SystemHolidays = () => {
   };
 
   function getDates(startDate, stopDate) {
-    var dateArray = new Array();
-    var currentDate = startDate;
+    let dateArray = new Array();
+    let currentDate = startDate;
     while (currentDate <= stopDate) {
-      dateArray.push({ holidate: new Date(currentDate), remarks: "" });
+      dateArray.push({ ...holidate, holidayDate: currentDate.toISOString() });
       currentDate = currentDate.addDays(1);
     }
     return dateArray;
@@ -61,26 +90,50 @@ const SystemHolidays = () => {
     console.log(workingDates);
   }, [workingDates]);
 
+  const HolidayValidator = (datesArray) => {
+    const result = {};
+    if (datesArray.length <= 0) {
+      result.date = "Choose a Date";
+    }
+    if (!holidate.remarks.trim("")) {
+      result.remarks = "A holiday Date must have a remark";
+    }
+    return result;
+  };
+
   useEffect(() => {
     if (endDate !== null) {
       setWorkingDates(getDates(startDate, endDate));
       return;
     } else if (startDate) {
-      const holidate = {
-        holidate: startDate,
-        remarks: "",
-      };
-      setWorkingDates((previousStates) => [holidate]);
+      setWorkingDates((previousStates) => [
+        ...previousStates,
+        { ...holidate, holidayDate: startDate.toISOString() },
+      ]);
     }
   }, [startDate, endDate]);
 
   const onChange = (dates) => {
     const [start, end] = dates;
+    const { date, ...newErrors } = errors;
+    setErrors(newErrors);
     setStartDate(start);
     setEndDate(end);
   };
 
-  return true ? (
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const { remarks, ...newErrors } = errors;
+    setErrors(newErrors);
+    if (workingDates.length > 0) {
+      workingDates.forEach((holiday) => {
+        holiday.remarks = value;
+      });
+    }
+    setHolidate({ ...holidate, [name]: value });
+  };
+
+  return holidays.length > 0 ? (
     <div className="individual-customer-form">
       <div className="lower-form-section">
         <div className="maintenance-customer-info">
@@ -94,7 +147,13 @@ const SystemHolidays = () => {
             <div className="set-holiday-section">
               <div className="calender-section">
                 <div className="input-div">
-                  <div className="label-holiday">Choose a Day(s)</div>
+                  <div
+                    className={
+                      errors.date ? "required label-holiday" : "label-holiday"
+                    }
+                  >
+                    Choose a Day(s)
+                  </div>
                   <div className="input-box">
                     <DatePicker
                       selected={startDate}
@@ -118,16 +177,20 @@ const SystemHolidays = () => {
                 <div className="input-div">
                   <div
                     className={
-                      false ? "required label-holiday" : "label-holiday"
+                      errors.remarks
+                        ? "required label-holiday"
+                        : "label-holiday"
                     }
                   >
                     Remarks
                   </div>
                   <div className="input-box">
                     <Input
-                      name="roleID"
+                      name="remarks"
                       type="text"
+                      value={holidate.remarks}
                       className="text-input-holiday"
+                      handleChange={handleChange}
                     />
                   </div>
                 </div>
@@ -135,7 +198,11 @@ const SystemHolidays = () => {
               <div className="buttons-add-holiday">
                 <div className="holiday-buttons-section">
                   <div className="holiday-buttons-section-container">
-                    <Button disabled={workingDates.length <= 0} name="Add" />
+                    <Button
+                      // disabled={workingDates.length <= 0}
+                      onClick={createSystemHolidays}
+                      name="Add"
+                    />
                     <Button name="Edit" />
                     <Button name="Remove" />
                   </div>
@@ -163,7 +230,7 @@ const SystemHolidays = () => {
     </div>
   ) : (
     <div className="spinner section">
-      <Loader />
+      <Loader headerText="Public Holidays: Holidays" />
     </div>
   );
 };
