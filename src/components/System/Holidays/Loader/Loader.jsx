ee34ee6140061c527dffc10e7660edc754e18207
onenfelix <onenfelix90@gmail.com>
2021-06-30 20:59:42 +0300

    Created a Loader for the Public Holidays section

---
+++ src/components/System/Holidays/Loader/Loader.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Spinner from "../../../Spinner/Spinner";
+import HolidaySidebar from "../HolidaySideBar";
+import "./index.css";
+
+function Loader({ headerText }) {
+  return (
+    <div className="view-individual-customer-form">
+      <div className="lower-form-section">
+        <div className="maintenance-customer-info">
+          <span>{headerText}</span>
+        </div>
+        <div className="lower-downer-section">
+          <div className="left-inner-form-section update-side-loader-height">
+            <HolidaySidebar />
+          </div>
+          <div className="submit-form-top-section loader-middle-section">
+            <div className="loader-middle-section-spinner">
+              <Spinner />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
