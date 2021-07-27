144a1a5cf7571dcd6030f67bf4bfada959656d2e
OdongAlican <sandieo.2020@gmail.com>
2021-07-27 21:31:03 +0300

    update api request

--- src/utils/api.js
+++ src/utils/api.js
@@ -141,7 +141,10 @@ export const UpdateCompanyInfoRequest = async (method, result, path) => {
   return response;
 };
 
-// export const FetchSystemModuleRequest = async (method, path) => {
+export const FetchSystemModuleRequest = async (method, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`);
+  return response;
+};
 
 export const GetGeneralLedgerSubTypes = async (method, path) => {
   const response = await axios[method](`${baseUrl}/${path}`);
@@ -158,7 +161,11 @@ export const PostSystemsRoleRequest = async (method, path, data) => {
   return response;
 };
 
-// export const UpdateSystemsRoleRequest = async (method, path, data) => {
+export const UpdateSystemsRoleRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
+
 export const PostGeneralLedgerSubTypes = async (method, path, data) => {
   const response = await axios[method](`${baseUrl}/${path}`, data);
   return response;
