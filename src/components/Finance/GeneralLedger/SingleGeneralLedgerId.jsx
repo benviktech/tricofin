d077945107c3d4d8f03bd37e2d5a8e01ee1a0a2e
OdongAlican <sandieo.2020@gmail.com>
2021-07-29 21:21:07 +0300

    update layout

--- src/components/Finance/GeneralLedger/SingleGeneralLedgerId.jsx
+++ src/components/Finance/GeneralLedger/SingleGeneralLedgerId.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
-import { getGeneralLedgerID } from '../../../actions/generalLedger';
+import { getGeneralLedgerID, updateGeneralLedgerID } from '../../../actions/generalLedger';
 import { GeneralLedgerSidebar } from '../../Sidebar/Sidebar';
 import IdModal from './IdModal';
 import fetchData from './fetchData';
@@ -10,8 +10,10 @@ import fetchData from './fetchData';
 const SingleGeneralLedgerId = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-
+  const [update, setUpdate] = useState(false);
   const generalLedgerID = useSelector(state => state.generalLedgerReducer.generalLedgerID);
+  const [updateData, setUpdateData] = useState({});
+  const [errors, setErrors] = useState({});
 
   const { glTypes, glSubTypes } = fetchData();
 
@@ -39,6 +41,51 @@ const SingleGeneralLedgerId = () => {
     return result;
   };
 
+  const updataState = e => {
+    e.preventDefault();
+    setUpdateData(generalLedgerID);
+    setUpdate(true);
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setUpdateData({
+      ...updateData,
+      [name]: value,
+    });
+  };
+
+  const cancelUpdate = () => {
+    setUpdateData({});
+    setUpdate(false);
+  };
+
+  const saveUpdate = e => {
+    e.preventDefault();
+    const formErrors = {};
+    formErrors.state = 'Updating';
+    if (!updateData.glType) {
+      formErrors.glType = 'GL type required';
+    }
+    if (!updateData.glName) {
+      formErrors.glName = 'Name is required';
+    }
+    if (!updateData.glSubType) {
+      formErrors.glSubType = 'GL sub type is required';
+    }
+    setErrors(formErrors);
+  };
+
+  useEffect(async () => {
+    if (Object.values(errors).includes('Updating')) {
+      if (Object.keys(errors).length === 1) {
+        await dispatch(updateGeneralLedgerID(updateData));
+        setUpdateData({});
+        setUpdate(false);
+      }
+    }
+  }, [errors]);
+
   return (
     <div className="individual-customer-form">
       <div className="lower-form-section">
@@ -64,7 +111,23 @@ const SingleGeneralLedgerId = () => {
                     <div className="left-subtypes-input">
                       <div className="id-information-section">
                         {
-                          generalLedgerID.glType && displayID(generalLedgerID.glType)
+                          Object.keys(updateData).length > 0 ? (
+                            <select
+                              className="w-100 pl-0"
+                              onChange={handleChange}
+                              name="glType"
+                              value={updateData.glType}
+                            >
+                              <option value="" disabled selected hidden>Select</option>
+                              {
+                                glTypes.map(type => (
+                                  <option key={type.glTypeID} value={type.glTypeID}>
+                                    {type.glType}
+                                  </option>
+                                ))
+                              }
+                            </select>
+                          ) : (displayID(generalLedgerID.glType))
                         }
                       </div>
                     </div>
@@ -73,40 +136,82 @@ const SingleGeneralLedgerId = () => {
                 <div className="right-subtypes-section">
                   <div className="left-inner-subtypes-section">
                     <div className="left-subtypes-span">GL Name:</div>
-                    <div className="left-subtypes-input">
-                      <div className="id-information-section">{generalLedgerID.glName}</div>
-                    </div>
+                    {
+                      Object.keys(updateData).length > 0 && update ? (
+                        <div className="left-subtypes-input">
+                          <input
+                            name="glName"
+                            onChange={handleChange}
+                            value={updateData.glName}
+                            type="text"
+                          />
+                          <div className="form-error-section-update">
+                            { errors.glName && errors.glName}
+                            {' '}
+                          </div>
+                        </div>
+                      ) : (
+                        <div className="left-subtypes-input">
+                          <div className="id-information-section">{generalLedgerID.glName}</div>
+                        </div>
+                      )
+                    }
                   </div>
                   <div className="left-inner-subtypes-section">
                     <div className="left-subtypes-span">GL Sub Type:</div>
                     <div className="left-subtypes-input">
                       <div className="id-information-section">
                         {
-                          generalLedgerID.glType && displaySubID(generalLedgerID.glSubType)
+                          Object.keys(updateData).length > 0 && update ? (
+                            <select
+                              className="w-100 pl-0"
+                              onChange={handleChange}
+                              name="glSubType"
+                              value={updateData.glSubType}
+                            >
+                              <option value="" disabled selected hidden>Select</option>
+                              {
+                                glSubTypes.map(type => (
+                                  <option key={type.glSubTypeID} value={type.glSubTypeID}>
+                                    {type.glSubType}
+                                  </option>
+                                ))
+                              }
+                            </select>
+                          ) : (displaySubID(generalLedgerID.glSubType))
                         }
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="submit-section">
-                <button type="submit"> Update </button>
-                <Link
-                  type="button"
-                  className="ml-1 edit-button"
-                  to={{
-                    pathname: '/glidentification',
-                  }}
-                >
-                  Cancel
-                </Link>
-              </div>
+              {
+                update ? (
+                  <div className="submit-section">
+                    <button type="submit" onClick={saveUpdate}> Save </button>
+                    <button type="submit" onClick={cancelUpdate}> Cancel </button>
+                  </div>
+                ) : (
+                  <div className="submit-section">
+                    <button type="submit" onClick={updataState}> Update </button>
+                    <Link
+                      type="button"
+                      className="ml-1 edit-button"
+                      to={{
+                        pathname: '/glidentification',
+                      }}
+                    >
+                      Cancel
+                    </Link>
+                  </div>
+                )
+              }
             </form>
             {
-                Object.keys(generalLedgerID).length > 0
-                  ? (
-                    <IdModal generalLedgerID={generalLedgerID} />
-                  ) : null
+              Object.keys(generalLedgerID).length > 0
+                ? (
+                  <IdModal generalLedgerID={generalLedgerID} />
+                ) : null
             }
           </div>
         </div>
