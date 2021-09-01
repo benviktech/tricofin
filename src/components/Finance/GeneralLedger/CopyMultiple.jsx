7af4cb93443a22adba9b6b9083f18b4611393028
OdongAlican <sandieo.2020@gmail.com>
2021-09-01 16:50:56 +0300

    update component layout

--- src/components/Finance/GeneralLedger/CopyMultiple.jsx
+++ src/components/Finance/GeneralLedger/CopyMultiple.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const CopyMultiple = () => {
-  const data = [{ BrID: '001', accountID: '0011000010005', AccountName: 'TELLER NAME' },
-    { BrID: '001', accountID: '0011000010005', AccountName: 'TELLER NAME' },
-    { BrID: '001', accountID: '0011000010005', AccountName: 'TELLER NAME' },
-    { BrID: '001', accountID: '0011000010005', AccountName: 'TELLER NAME' }];
+  const [branchList, setBranchList] = useState([]);
+  const [branchListFirst, setBranchListFirst] = useState([]);
+  const [values, setValues] = useState({});
+  const [secondValues, setSecondValues] = useState({});
+  const [ledgerAccounts, setLedgerAccounts] = useState([]);
+  const [currentBranchList, setCurrentBranchList] = useState([]);
+  const [currentBranchListFirst, setCurrentBranchListFirst] = useState([]);
+  const [globalState, setGlobalState] = useState(false);
+  const [finalSortedList, setFinalSortedList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://tricofin.azurewebsites.net/api/System/GetBranches')
+      .then(response => {
+        const updatedBranchList = [...response?.data, { branchID: '004', branchName: 'Global' }];
+        setBranchList(updatedBranchList);
+        setBranchListFirst(response?.data);
+      })
+      .catch(error => console.log(error.message));
+  }, []);
+
+  useEffect(async () => {
+    await axios.get('https://tricofin.azurewebsites.net/api/Finance/GetGeneralLedgers')
+      .then(response => setLedgerAccounts(response?.data))
+      .catch(error => console.log(error.message));
+  }, []);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSecondChange = e => {
+    const { name, value } = e.target;
+    setSecondValues({
+      ...secondValues,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    if (values.branch === '004') {
+      setCurrentBranchList(ledgerAccounts);
+      setGlobalState(true);
+    } else {
+      setGlobalState(false);
+      const result = ledgerAccounts.filter(element => element.branchID === values.branch);
+      setCurrentBranchList(result);
+    }
+  }, [values]);
+
+  useEffect(() => {
+    const result = ledgerAccounts.filter(element => element.branchID === secondValues.branch);
+    setCurrentBranchListFirst(result);
+  }, [secondValues]);
+
+  useEffect(() => {
+    if (currentBranchListFirst.length > 0 && currentBranchList.length > 0) {
+      const result = currentBranchList.filter(
+        a => !currentBranchListFirst.map(b => b.accountID).includes(a.accountID),
+      );
+      setFinalSortedList(result);
+    }
+  }, [currentBranchListFirst, currentBranchList]);
+
   return (
     <div className="main-copy-single-section">
       <div className="copy-single-section copy-single-section-many">
@@ -13,9 +77,31 @@ const CopyMultiple = () => {
             <span>From Branch:</span>
           </div>
           <div className="input-gl-account-id-section">
-            <select>
-              <option value="">value</option>
-            </select>
+            {
+              branchList.length > 0 ? (
+                <select
+                  name="branch"
+                  value={values.branch}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled selected hidden>Select</option>
+                  {
+                    branchList.map(branch => (
+                      <option
+                        key={branch.branchID}
+                        value={branch.branchID}
+                      >
+                        {branch.branchName}
+                      </option>
+                    ))
+                  }
+                </select>
+              ) : (
+                <select>
+                  <option value="" disabled selected hidden>Select</option>
+                </select>
+              )
+            }
           </div>
         </div>
         <div className="gl-branch-id-section gl-branch-id-section-many">
@@ -23,9 +109,31 @@ const CopyMultiple = () => {
             <span>To Branch:</span>
           </div>
           <div className="details-gl-branch-id-section">
-            <select>
-              <option value="">value</option>
-            </select>
+            {
+              branchListFirst.length > 0 ? (
+                <select
+                  name="branch"
+                  value={secondValues.branch}
+                  onChange={handleSecondChange}
+                >
+                  <option value="" disabled selected hidden>Select</option>
+                  {
+                    branchListFirst.map(branch => (
+                      <option
+                        key={branch.branchID}
+                        value={branch.branchID}
+                      >
+                        {branch.branchName}
+                      </option>
+                    ))
+                  }
+                </select>
+              ) : (
+                <select>
+                  <option value="" disabled selected hidden>Select</option>
+                </select>
+              )
+            }
           </div>
         </div>
         <button type="button" className="btn btn-success">
@@ -38,19 +146,30 @@ const CopyMultiple = () => {
           <div className="gl-exists-section-header">GL Exists</div>
           <div className="gl-exists-details-section">
             <div className="gl-exists-details-section-id bg-secondary-header">BrID</div>
-            <div className="gl-exists-details-section-accountId bg-secondary-header">AccountID</div>
-            <div className="gl-exists-details-section-account-name bg-secondary-header">Account Name</div>
+            <div className="gl-exists-details-section-accountId bg-secondary-header">
+              {globalState ? 'AccountID' : 'GLID'}
+            </div>
+            <div className="gl-exists-details-section-account-name bg-secondary-header">GLName</div>
           </div>
           <div className="main-gl-exists-details-section">
             {
-          data.map(value => (
-            <div key={value.BrID} className="gl-exists-details-section">
-              <div className="gl-exists-details-section-id">{value.BrID}</div>
-              <div className="gl-exists-details-section-accountId">{value.accountID}</div>
-              <div className="gl-exists-details-section-account-name">{value.AccountName}</div>
-            </div>
-          ))
-        }
+              currentBranchList.map(value => (
+                <div key={value.accountID} className="gl-exists-details-section">
+                  <div className="gl-exists-details-section-id">
+                    {globalState ? 'GLB' : value.branchID}
+                  </div>
+                  <div className="gl-exists-details-section-accountId">
+                    {globalState ? value.glid : value.accountID}
+                  </div>
+                  <div className="gl-exists-details-section-account-name">
+                    {
+                      value.accountName.length < 25 ? value.accountName
+                        : (`${value.accountName.substring(0, 25)} ...`)
+                      }
+                  </div>
+                </div>
+              ))
+            }
           </div>
         </div>
         <div className="gl-does-not-exist-section">
@@ -64,24 +183,29 @@ const CopyMultiple = () => {
             <div className="checkbox-label">Select All</div>
           </div>
           <div className="gl-does-not-exist-section-outter gl-does-not-exist-section-outter-many top-outer-gl-header-section">
-            <div className="gl-does-not-exist-section-first">BrID</div>
-            <div className="gl-does-not-exist-section-second">GLID</div>
-            <div className="gl-does-not-exist-section-second">GLName</div>
+            <div className="gl-does-not-exist-section-first d-flex justify-content-center">BrID</div>
+            <div className="gl-does-not-exist-section-second d-flex justify-content-center">GLID</div>
+            <div className="gl-does-not-exist-section-second d-flex justify-content-center">GLName</div>
           </div>
           <div className="main-gl-does-not-exist-section">
             {
-              data.map(value => (
-                <div key={value.BrID} className="gl-does-not-exist-section-outter gl-does-not-exist-section-outter-many">
-                  <div className="gl-does-not-exist-section-first gl-does-not-exist-section-first-loop">
+              finalSortedList.map(value => (
+                <div key={value.accountID} className="gl-does-not-exist-section-outter gl-does-not-exist-section-outter-many">
+                  <div className="gl-does-not-exist-section-first d-flex justify-content-center gl-does-not-exist-section-first-loop">
                     <div className="gl-does-not-exist-section-first-chechbox">
                       <input type="checkbox" />
                     </div>
                     <div className="gl-does-not-exist-section-first-brID">
-                      {value.BrID}
+                      {value.branchID}
                     </div>
                   </div>
-                  <div className="gl-does-not-exist-section-second">{value.AccountName}</div>
-                  <div className="gl-does-not-exist-section-second">{value.AccountName}</div>
+                  <div className="gl-does-not-exist-section-second d-flex justify-content-center">{value.glid}</div>
+                  <div className="gl-does-not-exist-section-second d-flex justify-content-center">
+                    {
+                      value.accountName.length < 20 ? value.accountName
+                        : (`${value.accountName.substring(0, 20)} ...`)
+                      }
+                  </div>
                 </div>
               ))
             }
