9f5619d652b1c8a0143a3d5c7ceecc3b4b9e653a
OdongAlican <sandieo.2020@gmail.com>
2021-09-27 17:23:03 +0300

    add the lower form section

--- src/components/Finance/GeneralLedgerPayments/GeneralLedgerPayments.jsx
+++ src/components/Finance/GeneralLedgerPayments/GeneralLedgerPayments.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './index.css';
 
 const GeneralLedgerPayments = () => {
-  console.log('generaledgerpayments');
   const description = [
     { id: 1, description: 'Cash at Hand' },
     { id: 2, description: 'Cash at Bank' },
-    { id: 1, description: 'InterBranch Account' },
-    { id: 1, description: 'Profit/Loss Reserve Account' },
+    { id: 3, description: 'InterBranch Account' },
+    { id: 4, description: 'Profit/Loss Reserve Account' },
   ];
   return (
     <div className="individual-customer-form">
@@ -16,14 +15,14 @@ const GeneralLedgerPayments = () => {
           <span>Main General Ledger Parameters</span>
         </div>
         <div className="lower-downer-section-gen-led">
-          <div className="first-ledger-parameters">SerialID</div>
-          <div className="second-ledger-parameters">Description</div>
-          <div className="third-ledger-parameters">GL Account ID</div>
-          <div className="forth-ledger-parameters">GL Account Name</div>
+          <div className="first-ledger-parameters text-white">SerialID</div>
+          <div className="second-ledger-parameters text-white">Description</div>
+          <div className="third-ledger-parameters text-white">GL Account ID</div>
+          <div className="forth-ledger-parameters text-white">GL Account Name</div>
         </div>
         {
           description.map(element => (
-            <div key={element.id} className="lower-downer-section-gen-led">
+            <div key={element.id} className="lower-downer-section-gen-led lower-downer-section-gen-led-lower">
               <div className="first-ledger-parameters">{ element.id }</div>
               <div className="second-ledger-parameters">Description</div>
               <div className="third-ledger-parameters">GL Account ID</div>
@@ -31,6 +30,24 @@ const GeneralLedgerPayments = () => {
             </div>
           ))
         }
+        <div className="gl-parameters-submission-form">
+          <div className="mb-2 gl-parameters-submission-form-header">GL Parameters Details</div>
+          <div className="gl-parameter-details-bottom">
+            <div className="inter-branch-left">GL Parameter :</div>
+            <div className="inter-branch-right">3 - InterBranch Account :</div>
+          </div>
+          <div className="gl-parameter-details-bottom">
+            <div className="inter-branch-left">GL AccountID :</div>
+            <input type="text" />
+            <div className="first-empty-parameter">1</div>
+            <div className="second-empty-parameter">2</div>
+          </div>
+        </div>
+        <div className="gl-parameters-button-section">
+          <button type="button">Edit</button>
+          <button type="button">Save</button>
+          <button type="button">Cancel</button>
+        </div>
       </div>
     </div>
   );
