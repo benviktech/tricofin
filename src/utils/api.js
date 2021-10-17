b275e69d3a61d58b8f0bde5808af6a914cf48abe
OdongAlican <sandieo.2020@gmail.com>
2021-10-18 00:00:48 +0300

    add api methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -254,3 +254,8 @@ export const FetchGLParametersRequest = async (method, path) => {
   const response = await axios[method](`${baseUrl}/${path}`);
   return response;
 };
+
+export const SaveTransactionRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
