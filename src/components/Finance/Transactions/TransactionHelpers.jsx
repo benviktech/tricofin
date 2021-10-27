45b0ea9ce4ec14b4d778c32dfc91432e2005ca1c
OdongAlican <sandieo.2020@gmail.com>
2021-10-27 16:06:25 +0300

    add transaction helpers component

---
+++ src/components/Finance/Transactions/TransactionHelpers.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-nested-ternary */
+
+export const GLListFilter = (glList, content, text) => {
+  const newModalList = []; let accId = '';
+  glList.forEach(account => {
+    if (text === 'accountId') { accId = account.accountID; }
+    if (text === 'accountName') { accId = account.accountName; }
+    if (accId.length > 0) {
+      if (accId.indexOf(content.toLocaleUpperCase()) !== -1) {
+        newModalList.push(account);
+      }
+    }
+  });
+
+  return newModalList;
+};
+
+export const SCListFilter = (savingsList, sharesList, content, type, text) => {
+  const newModalList = []; let accId = '';
+  (type === 'savings' ? savingsList : type === 'shares' ? sharesList : null)
+    .forEach(account => {
+      if (text === 'accountId') { accId = account.accountID; }
+      if (text === 'accountName') { accId = account.accountName; }
+      if (accId.length > 0) {
+        if (accId.indexOf(content.toLocaleUpperCase()) !== -1) {
+          newModalList.push(account);
+        }
+      }
+    });
+  return newModalList;
+};
+
+export const calculateTotal = (cashTransactionList, type) => {
+  const total = cashTransactionList
+    .filter(transaction => transaction.partTranType === type
+    && transaction.tranSerialNo === 1)
+    .reduce((sum, x) => sum + x.tranAmount, 0);
+  return total;
+};
