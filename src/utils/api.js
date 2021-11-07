1e72161b3d310aeaa3bc08b33c08fd2c8f6c86a4
OdongAlican <sandieo.2020@gmail.com>
2021-11-07 17:38:39 +0300

    update component

--- src/utils/api.js
+++ src/utils/api.js
@@ -289,3 +289,8 @@ export const UpdateFixedAssetsRequest = async (method, path, data) => {
   const response = await axios[method](`${baseUrl}/${path}`, data);
   return response;
 };
+
+export const PostMaintainFixedAsstRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
